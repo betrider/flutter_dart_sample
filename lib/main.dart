8f374170766e4baafcba7881dba38a2e62c8b472
betrider <betrider@example.com>
2021-02-06 12:39:46 +0900

    1.생성자 샘플 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,197 @@
 import 'package:flutter/material.dart';
 
+//리다이렉팅
+class Person3{
+  String name;
+  int age;
+
+  Person3(this.name, this.age){
+    print('person3 name:$name, age:$age');
+  }
+
+  Person3.initName(String name) : this(name, 20);
+}
+
+//팩토리
+class Person2{
+  Person2.init();
+
+  factory Person2(String type){
+    switch (type){
+      case 'Student':
+        return Student2();
+      case 'Employee':
+        return Employee();
+    }
+  }
+
+  String getType(){
+    return 'Person';
+  }
+}
+
+class Student2 extends Person2{
+  Student2() : super.init();
+
+  @override
+  String getType(){
+    return 'Student';
+  }
+}
+
+class Employee extends Person2{
+  Employee() : super.init();
+
+  @override
+  String getType(){
+    return 'Employee';
+  }
+}
+
+class Person{
+  String name;
+  int age;
+
+  //이름없는 생성자
+  Person() //생성자
+      : name='tt', age = 5{ //초기화
+   print('person 이름없는 생성자 name:$name, age:$age');
+  }
+
+  //이름있는 생성자
+  Person.init(this.name, this.age){
+    print('person.init 이름있는 생성자 name:$name, age:$age');
+  }
+}
+
+class Student extends Person{
+  Student(){
+    print('Student 이름없는 생성자 자신 name:${this.name} 자신 age:${this.age} 부모 name:${super.name} 부모 age:${super.age}');
+  }
+}
+
+void main(){
+  String name = '홍길동';
+  int age = 152;
+
+  var person = Person();
+  print('person 이름:${person.name} 나이:${person.age}');
+  print('------------------------------------------------');
+
+  var personInit = Person.init(name, age);
+  print('person2 이름:${personInit.name} 나이:${personInit.age}');
+  print('------------------------------------------------');
+
+  var student = Student();
+  print('------------------------------------------------');
+
+  var student2 = Person2('Student');
+  var employee = Person2('Student');
+
+  print('type = ${student2.getType()}');
+  print('type = ${employee.getType()}');
+
+  print('------------------------------------------------');
+
+  var person3 = Person3.initName('kim');
+
+}
+
+/*
+import 'package:flutter/material.dart';
+
 void main() {
-  runApp(MyApp());
+  String my_name = 'Park';
+  int my_age = 39;
+  String my_location = "Daejeon";
+
+  Class_A class_A = Class_A(my_name, my_age, my_location);
+  class_A.printInfo();
+
+  Class_B class_B = Class_B(age: my_age, name: my_name);
+  class_B.printInfo();
+
+  Class_C class_c1 = Class_C(name: my_name, age: my_age, location: my_location);
+  class_c1.printInfo();
+  Class_C class_c2 = Class_C(name: my_name, age: my_age);
+  class_c2.printInfo();
+  Class_C class_c3 = Class_C(name: my_name);
+  class_c3.printInfo();
+
+  Class_D class_d1 = Class_D(my_name, age: my_age, location: my_location);
+  class_d1.printInfo();
+  Class_D class_d2 = Class_D(my_name, location: my_location);
+  class_d2.printInfo();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+//필수인자, 순서상관있음
+class Class_A {
+  String name;
+  int age;
+  String location;
+
+  //1번 방법
+  */
+/*Class_A(String name, int age, String location) {
+    //this.name = name;
+    //this.age = age;
+    //this.location = location;
+  }*//*
+
+
+  //2번 방법
+  Class_A(this.name,this.age,this.location);
+
+  void printInfo() {
+    print('<Class_A> name: $name, age: $age');
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+//가변인자, 순서상관없음
+class Class_B {
+  String name;
+  int age;
+  String location;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Class_B({this.name, this.age, this.location});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void printInfo() {
+    print('<Class_B> name: $name, age: $age , location: $location');
   }
 }
+
+//가변인자, 순서상관없음
+class Class_C {
+  String name;
+  int age;
+  String location;
+
+  Class_C({this.name, this.age, this.location});
+
+  void printInfo() {
+    print('<Class_C> name: $name, age: $age, location: $location');
+  }
+}
+
+//가변인자, 순서상관없음, required는 있어도 작동함(assert 추가 필요)
+class Class_D {
+  String name;
+  int age;
+  String location;
+
+  //1번
+  */
+/*Class_D(String name, {this.age, @required this.location})
+      : assert(location != null){
+    this.name = name;
+  }*//*
+
+
+  //2번
+  Class_D(this.name, {this.age, @required this.location})
+      : assert(location != null);
+
+  void printInfo() {
+    print('<Class_D> name: $name, age: $age, location: $location');
+  }
+}*/
